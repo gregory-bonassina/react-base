6e4c4dd12b13842613081a837e0c5aab036aa5c9
Gregory Bonassina <bonassina3@gmail.com>
2021-09-19 18:06:52 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 'off',
+    'import/prefer-default-export': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ src/pages/Login/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  background: green;
+
+  small {
+    font-size: 12pt;
+    margin-left: 12px;
+  }
+`;
+
+export const Paragraph = styled.p`
+  font-size: 80px;
+`;
